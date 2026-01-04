dd9047f3c345858f8d1b77c5fc2d73be2a755355
oTAETAEo <ggg7515@naver.com>
2026-01-05 02:11:58 +0900

    feat(AuditingConfig) : Auditing 설정 분리 및 적용
    - 슬라이스 테스트 용이성을 위한 분리

--- src/main/java/hexa/chat/ChatApplication.java
+++ src/main/java/hexa/chat/ChatApplication.java
@@ -2,9 +2,7 @@ package hexa.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class ChatApplication {
 
