7f10ae18091dcdc036f3c4de90f3c6d3895a3076
oTAETAEo <ggg7515@naver.com>
2026-01-09 17:51:58 +0900

    refactor(auth) : 로그인 인터페이스 매개변수 @Valid 추가 및 다중 로그인 테스트 추가

--- src/main/java/hexa/chat/application/auth/provided/LoginUseCase.java
+++ src/main/java/hexa/chat/application/auth/provided/LoginUseCase.java
@@ -2,10 +2,12 @@ package hexa.chat.application.auth.provided;
 
 import hexa.chat.application.auth.dto.LoginRequest;
 import hexa.chat.application.auth.dto.LoginResponse;
+
 import jakarta.annotation.Nullable;
+import jakarta.validation.Valid;
 
 public interface LoginUseCase {
 
-    LoginResponse login(LoginRequest request, @Nullable String deviceId);
+    LoginResponse login(@Valid LoginRequest request, @Nullable String deviceId);
 
 }
--- src/test/java/hexa/chat/application/auth/provided/LoginUseCaseTest.java
+++ src/test/java/hexa/chat/application/auth/provided/LoginUseCaseTest.java
@@ -2,34 +2,33 @@ package hexa.chat.application.auth.provided;
 
 import hexa.chat.application.auth.dto.LoginRequest;
 import hexa.chat.application.auth.dto.LoginResponse;
-import hexa.chat.application.auth.required.TokenProvider;
+import hexa.chat.application.auth.required.RefreshTokenRepository;
 import hexa.chat.application.member.required.MemberRepository;
-import hexa.chat.domain.auth.InvalidCredentialsException;
+import hexa.chat.domain.auth.refreshToken.RefreshToken;
 import hexa.chat.domain.member.Member;
 import hexa.chat.domain.member.MemberFixture;
 import hexa.chat.domain.member.PasswordEncoder;
-import hexa.chat.domain.shared.Token;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import jakarta.persistence.EntityManager;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
-@ActiveProfiles("test")
 class LoginUseCaseTest {
 
     @Autowired
@@ -40,6 +39,9 @@ class LoginUseCaseTest {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
 
     @Autowired
     private EntityManager entityManager;
@@ -73,9 +75,33 @@ class LoginUseCaseTest {
         assertThat(member.getPublicId().toString()).isEqualTo(claims.getSubject());
     }
 
-    @DisplayName("로그인 실패 한다.")
+    @DisplayName("다른 기기로 이중 로그인을 한다.")
     @Test
-    void loginFail () {
+    void loginAgainWithSameAccountOnDifferentDevice () {
+        // given
+        Member member = memberRepository.save(MemberFixture.createMember("test@test.com", "test1234", passwordEncoder));
+
+        LoginRequest loginRequest1 = new LoginRequest("test@test.com", "test1234");
+        LoginRequest loginRequest2 = new LoginRequest("test@test.com", "test1234");
+
+        // when
+        LoginResponse response1 = loginUseCase.login(loginRequest1, "123456789");
+        LoginResponse response2 = loginUseCase.login(loginRequest2, null);
+        LoginResponse response3 = loginUseCase.login(loginRequest1, "");
+
+        // then
+        entityManager.flush();
+        entityManager.clear();
+
+        List<RefreshToken> result = refreshTokenRepository.findAllByMemberId(member.getId());
+        assertThat(result).hasSize(3)
+            .extracting("deviceId")
+            .containsExactlyInAnyOrder(response1.deviceId(), response2.deviceId(), response3.deviceId());
+    }
+
+    @DisplayName("비밀번호 불일치로 로그인 실패 한다.")
+    @Test
+    void loginFail1 () {
         // given
         memberRepository.save(MemberFixture.createMember("test@test.com", "test1234", passwordEncoder));
 
@@ -83,7 +109,18 @@ class LoginUseCaseTest {
 
         // when - then
         assertThatThrownBy(() -> loginUseCase.login(loginRequest, null))
-            .isInstanceOf(InvalidCredentialsException.class);
+            .isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @DisplayName("회원이 존재하지 않아 로그인 실패 한다.")
+    @Test
+    void loginFail2 () {
+        // given
+        LoginRequest loginRequest = new LoginRequest("test@test.com", "test1234");
+
+        // when - then
+        assertThatThrownBy(() -> loginUseCase.login(loginRequest, null))
+            .isInstanceOf(NoSuchElementException.class);
     }
 
 }
