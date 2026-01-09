9ec9a637da8f0d693eb53fe0f0a01fb489e49f46
oTAETAEo <ggg7515@naver.com>
2026-01-09 17:46:29 +0900

    feat(auth) : 회원 아이디를 통해 토큰 조회 기능 추가

--- src/main/java/hexa/chat/application/auth/required/RefreshTokenRepository.java
+++ src/main/java/hexa/chat/application/auth/required/RefreshTokenRepository.java
@@ -3,6 +3,7 @@ package hexa.chat.application.auth.required;
 import hexa.chat.domain.auth.refreshToken.RefreshToken;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends Repository<RefreshToken, Long> {
@@ -12,5 +13,7 @@ public interface RefreshTokenRepository extends Repository<RefreshToken, Long> {
     Optional<RefreshToken> findById(Long id);
 
     Optional<RefreshToken> findByMemberIdAndDeviceId(Long memberId, String deviceId);
+
+    List<RefreshToken> findAllByMemberId(Long memberId);
 
 }
