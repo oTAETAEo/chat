cd6cd28981554b75dab3efbb184f32dc3b3a5845
oTAETAEo <ggg7515@naver.com>
2026-01-09 17:58:06 +0900

    feat(security) : 시큐리티 검증 제외 경로 추가

--- src/main/java/hexa/chat/adapter/security/SecurityConfig.java
+++ src/main/java/hexa/chat/adapter/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package hexa.chat.adapter.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,7 +23,7 @@ public class SecurityConfig {
 
             .authorizeHttpRequests((auth) -> auth
                 .requestMatchers(
-                    "/login"
+                    "/login", "/sign-up"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
