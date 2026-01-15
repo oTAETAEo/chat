535edb41e40e8c721fea70b33a99c216f701a76a
oTAETAEo <ggg7515@naver.com>
2026-01-16 02:24:06 +0900

    feat: 로그인 시 최소한의 검증 기능만 작동 하도록 변경

--- src/main/java/hexa/chat/application/auth/dto/LoginRequest.java
+++ src/main/java/hexa/chat/application/auth/dto/LoginRequest.java
@@ -1,6 +1,5 @@
 package hexa.chat.application.auth.dto;
 
-import hexa.chat.domain.auth.rule.PasswordRule;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
@@ -10,7 +9,7 @@ public record LoginRequest(
     @Email(message = "이메일 형식에 맞지 않습니다.")
     String email,
 
-    @PasswordRule
+    @NotBlank(message = "비밀번호를 입력 해 주세요.")
     String password
 
 ) {
