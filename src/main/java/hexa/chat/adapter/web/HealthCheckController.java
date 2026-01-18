4b846b6c05853321a968e9d3fd27844aab88981b
oTAETAEo <ggg7515@naver.com>
2026-01-19 02:05:20 +0900

    feat : 배포 체크 컨트롤러 추가

---
+++ src/main/java/hexa/chat/adapter/web/HealthCheckController.java
@@ -0,0 +1,42 @@
+package hexa.chat.adapter.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+@RestController
+public class HealthCheckController {
+
+    @Value("${server.env}")
+    private String env;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${server.address:}")
+    private String address;
+
+    @Value("${serverName}")
+    private String serverName;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new TreeMap<>();
+        responseData.put("serverName", serverName);
+        responseData.put("address", address);
+        responseData.put("port", port);
+        responseData.put("env", env);
+
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}
